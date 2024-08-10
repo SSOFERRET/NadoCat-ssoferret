b7739893ef85c9a5583fbffddb94d2026fd2ed8b
ssoferret <owosso1212@gmail.com>
2024-08-10 19:02:39 +0900

    Feat: createMissingReport를 라우터에 추가

--- server/routes/missings.ts
+++ server/routes/missings.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { getMissingFavorites, postMissingFavorites, deleteMissingFavorites } from "../controller/missing/MissingsFavorites";
 import { getMissings } from "../controller/missing/Missings";
 import { createMissing, deleteMissing } from "../controller/missing/Missing";
+import { createMissingReport } from "../controller/missing/MissingReport";
 
 const router = express.Router();
 router.use(express.json());
@@ -9,6 +10,9 @@ router.use(express.json());
 router.get("", getMissings);
 router.post("", createMissing);
 router.delete("/:postId", deleteMissing);
+
+router.post("/:postId/reports", createMissingReport);
+
 router.get("/favorites", getMissingFavorites);
 router.post("/favorites", postMissingFavorites);
 router.delete("/favorites/:postId", deleteMissingFavorites);
