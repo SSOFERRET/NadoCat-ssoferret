73d0751d7fcee13fca22902bce7556502a6eec01
ssoferret <owosso1212@gmail.com>
2024-08-11 18:31:47 +0900

    Feat: 실종 고양이 제보글 UPDATE 함수 작성

--- server/controller/missing/MissingReport.ts
+++ server/controller/missing/MissingReport.ts
@@ -2,8 +2,8 @@ import prisma from "../../client";
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { Prisma } from "@prisma/client";
-import { addLocation } from "../../model/location.model";
-import { addLocationFormats, addMissingReport, removePost } from "../../model/missing.model";
+import { addLocation, updateLocationById } from "../../model/location.model";
+import { addLocationFormats, addMissingReport, getPostByPostId, removePost, updateMissingReportByPostId } from "../../model/missing.model";
 import { getUserId, validateError } from "./Missing";
 
 import { CATEGORY } from "../../constants/category";
@@ -105,6 +105,48 @@ export const deleteMissingReportHandler = async (req: Request, res: Response) =>
   try {
     deleteMissingReport(req, res);
   } catch (error) {
+    if (error instanceof Error)
+      return validateError(res, error);
+  }
+};
+
+export const updateMissingReport = async (req: Request, res: Response) => { // NOTE Full Update?인지 확인
+  try {
+    const postId = Number(req.params.postId);
+    const userId = await getUserId(); // NOTE
+    const { report, location, images } = req.body;
+    const postData = {
+      postId,
+      userId,
+      categoryId: CATEGORY.MISSING_REPORTS
+    }
+
+    await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
+      const post = await getPostByPostId(tx, postData);
+      if (!post || !report || !location)
+        return res.status(StatusCodes.BAD_REQUEST).json({ message: "입력값 확인 요망" });
+
+      const locationId = post.locationId;
+      if (locationId)
+        await updateLocationById(tx, locationId, location);
+
+      await updateMissingReportByPostId(tx, postId, userId, report.detail, new Date(report.time))
+
+      const imagesToDelete = await getAndDeleteImageFormats(tx, postData);
+
+      if (imagesToDelete)
+        await deleteImagesByImageIds(tx, imagesToDelete);
+
+      if (images) {
+        await addNewImages(tx, postData, images)
+      };
+    })
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: "게시글이 수정되었습니다." });
+  } catch (error) {
+    console.log(error);
     if (error instanceof Error)
       return validateError(res, error);
   }
