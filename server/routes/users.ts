8a68ed060063544a5208b7610905f853dcaa5d9d
dogfootdragon <dogfootdragon@gmail.com>
2024-08-12 02:08:41 +0900

    Feat: 동네 고양이 도감 API 구현

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,10 +1,23 @@
 import express, { Router } from "express";
 const router: Router = express.Router();
 import signup from "../controller/user/Users";
+import {
+  getFavoriteCats,
+  getFavoriteCat,
+  addFavoriteCat,
+  deleteFavoriteCat
+} from "../controller/streetCat/StreetCatsFavorite"
 
 router.use(express.json());
 
 //회원가입
 router.post("/signup", signup);
+
+
+// 동네 고양이 도감 즐겨찾기(내 도감)
+router.get("/street-cats", getFavoriteCats);
+router.get("/street-cats/:street_cat_id", getFavoriteCat);
+router.post("/street-cats/:street_cat_id", addFavoriteCat);
+router.delete("/street-cats/:street_cat_id", deleteFavoriteCat);
 
 export default router;
