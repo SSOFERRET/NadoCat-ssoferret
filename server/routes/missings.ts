5a723a1156c70905eea7aeb6707369e987b0e92f
ssoferret <owosso1212@gmail.com>
2024-08-11 19:31:06 +0900

    Feat: 실종 고양이 제보글 일치 여부 UPDATE 함수 라우터에 추가

--- server/routes/missings.ts
+++ server/routes/missings.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { getMissingFavorites, postMissingFavorites, deleteMissingFavorites } from "../controller/missing/MissingsFavorites";
 import { getMissings } from "../controller/missing/Missings";
 import { createMissing, deleteMissing, updateFoundState, updateMissing } from "../controller/missing/Missing";
-import { createMissingReport, deleteMissingReport, deleteMissingReportHandler, updateMissingReport } from "../controller/missing/MissingReport";
+import { createMissingReport, deleteMissingReport, deleteMissingReportHandler, updateMissingReport, updateMissingReportCheck } from "../controller/missing/MissingReport";
 
 const router = express.Router();
 router.use(express.json());
@@ -11,11 +11,12 @@ router.get("", getMissings);
 router.post("", createMissing);
 router.delete("/:postId", deleteMissing);
 router.put("/:postId", updateMissing);
-router.patch("/:postId/found", updateFoundState);
+router.patch("/:postId", updateFoundState);
 
 router.post("/:postId/reports", createMissingReport);
 router.delete("/:MissingId/reports/:postId", deleteMissingReportHandler);
 router.put("/:missingId/reports/:postId", updateMissingReport);
+router.patch("/:missingId/reports/:postId", updateMissingReportCheck);
 
 router.get("/favorites", getMissingFavorites);
 router.post("/favorites", postMissingFavorites);
