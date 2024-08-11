4ca3287fa1863fd73200d5a5c59277e8d63935f5
ssoferret <owosso1212@gmail.com>
2024-08-11 11:32:32 +0900

    Feat: 실종고양이 게시글 UPDATE 기능라우터에 추가

--- server/routes/missings.ts
+++ server/routes/missings.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { getMissingFavorites, postMissingFavorites, deleteMissingFavorites } from "../controller/missing/MissingsFavorites";
 import { getMissings } from "../controller/missing/Missings";
-import { createMissing, deleteMissing } from "../controller/missing/Missing";
+import { createMissing, deleteMissing, updateMissing } from "../controller/missing/Missing";
 import { createMissingReport, deleteMissingReport } from "../controller/missing/MissingReport";
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.use(express.json());
 router.get("", getMissings);
 router.post("", createMissing);
 router.delete("/:postId", deleteMissing);
+router.put("/:postId", updateMissing);
 
 router.post("/:postId/reports", createMissingReport);
 router.delete("/:MissingId/reports/:postId", deleteMissingReport);
