5419ba05381e271591588f6ee1bdf66c21dc2fa0
Minep <pmh@ge.kornu.ac.kr>
2024-08-11 17:56:04 +0900

    Refactor: 로그인에 jwt 추가

--- server/controller/user/Users.ts
+++ server/controller/user/Users.ts
@@ -1,34 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
-import {StatusCodes} from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 import dotenv from "dotenv";
 dotenv.config();
 import bcryto from "bcrypt";
 // import crypto from "crypto";
 import { v4 as uuidv4 } from "uuid";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const uuid = uuidv4();
 const uuidBuffer = Buffer.from(uuid.replace(/-/g, ""), "hex");
 console.log("uuidBuffer:", uuidBuffer);
-
 const prisma = new PrismaClient();
 
 //[x]회원가입
 export const signup = async (req: Request, res: Response) => {
-  const {email, nickname, password, authtype} = req.body;
+  const { email, nickname, password, authtype } = req.body;
 
   const hashing = async (password: string) => {
-    const saltRound = 10; 
+    const saltRound = 10;
     const salt = await bcryto.genSalt(saltRound);
 
     const hashPassword = await bcryto.hash(password, salt);
-    return {salt, hashPassword};
-  }
+    return { salt, hashPassword };
+  };
 
   //DB저장
-  try{
-    const {salt, hashPassword} = await hashing(password);
+  try {
+    const { salt, hashPassword } = await hashing(password);
 
     const result = await prisma.$transaction(async (prisma) => {
       const user = await prisma.users.create({
@@ -37,7 +36,7 @@ export const signup = async (req: Request, res: Response) => {
           email: email,
           nickname: nickname,
           auth_type: authtype,
-          status: "active",  //default: active
+          status: "active", //default: active
         },
       });
 
@@ -49,14 +48,17 @@ export const signup = async (req: Request, res: Response) => {
         },
       });
 
-      return {user, secretUser};
-    })
-    
+      return { user, secretUser };
+    });
+
     //변환 확인용으로 넣음
-    const userUuidString = result.user.uuid.toString("hex").match(/.{1,4}/g)?.join("-");
+    const userUuidString = result.user.uuid
+      .toString("hex")
+      .match(/.{1,4}/g)
+      ?.join("-");
     console.log("userUuidString:", userUuidString);
 
-    if(result.user && result.secretUser){
+    if (result.user && result.secretUser) {
       return res.status(StatusCodes.CREATED).json({
         message: "회원가입 성공!",
         user: {
@@ -65,98 +67,109 @@ export const signup = async (req: Request, res: Response) => {
           email: result.user.email,
           nickname: result.user.nickname,
           // authtype: result.user.auth_type
-        }
+        },
       });
-    }else{
-      return res.status(StatusCodes.BAD_REQUEST).json({message: "회원가입 실패!"});
+    } else {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "회원가입 실패!" });
     }
-      
-  }catch(error){
+  } catch (error) {
     console.log("회원가입 error:", error);
     return res.status(StatusCodes.BAD_REQUEST).end();
   }
 };
 
-
 //[x]로그인
-//[ ]jwt 어떻게 할건지
 export const login = async (req: Request, res: Response) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
 
   try {
     const result = await prisma.$transaction(async (prisma) => {
       const selectUsers = await prisma.users.findFirst({
         where: {
-          email: email 
-        }
+          email: email,
+        },
       });
 
-      if(!selectUsers) {//사용자 없을 때1
-        // return res.status(StatusCodes.BAD_REQUEST).json({message: "사용자를 찾을 수 없습니다."});
+      if (!selectUsers) {
         console.log("사용자를 찾을 수 없습니다.");
         return null;
       }
-      
-      const userUuid = selectUsers.uuid; //uuid가져옴
+
+      const userUuid = selectUsers.uuid; 
       const selectUserSecrets = await prisma.user_secrets.findFirst({
         where: {
-          uuid: userUuid
-        }
+          uuid: userUuid,
+        },
       });
-      
-      if(!selectUserSecrets) { //사용자 없을 때2
-        // return res.status(StatusCodes.BAD_REQUEST).json({message: "사용자를 찾을 수 없습니다."});
+
+      if (!selectUserSecrets) {
         console.log("사용자를 찾을 수 없습니다.");
-        return null; 
-      }
-      
-      return {selectUsers, selectUserSecrets};
-      }); //result 끝
-      
-      if(!result){ //사용자가 없다면
-        return res.status(StatusCodes.BAD_REQUEST).json({message: "사용자를 찾을 수 없습니다."});
-      }
-      
-      //비밀번호 검증
-      const {selectUsers, selectUserSecrets} = result; //구조분해할당
-
-      // const salt = selectUserSecrets.salt;
-      // const hashPassword = await bcryto.hash(password, salt);
-      const isPasswordValid = await bcryto.compare(password, selectUserSecrets.hash_password); //비밀번호 검증
-      if(!isPasswordValid){
-      return res.status(StatusCodes.BAD_REQUEST).json({message: "사용자 정보가 일치하지 않습니다."});
+        return null;
       }
 
-      const userUuidString = selectUsers.uuid.toString("hex").match(/.{1,4}/g)?.join("-"); //string변경
+      return { selectUsers, selectUserSecrets };
+    })
 
-      return res.status(StatusCodes.OK).json({
-        message: "로그인 성공",
-        user: { 
-          email: selectUsers.email,
-          nickname: selectUsers.nickname,
-          password: selectUserSecrets.hash_password,
-          uuid: userUuidString
-        }
+    if (!result) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "사용자를 찾을 수 없습니다." });
+    }
+
+    const { selectUsers, selectUserSecrets } = result; 
+    const isPasswordValid = await bcryto.compare(
+      password,
+      selectUserSecrets.hash_password
+    ); 
+
+    if (!isPasswordValid) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "사용자 정보가 일치하지 않습니다." });
+    }
+
+    const userUuidString = selectUsers.uuid.toString("hex").match(/.{1,4}/g) ?.join("-"); 
+    const token = jwt.sign(
+      { 
+        uuid: userUuidString,
+        email: selectUsers.email
+      }, process.env.PRIVATE_KEY as string, {
+        expiresIn: "1m",
+        issuer: "fefive"
       });
 
+      res.cookie("token", token, {
+        httpOnly: true
+      });
+
+    return res.status(StatusCodes.OK).json({
+      message: "로그인 성공",
+      user: {
+        email: selectUsers.email,
+        nickname: selectUsers.nickname,
+        password: selectUserSecrets.hash_password,
+        uuid: userUuidString,
+      },
+    });
   } catch (error) {
     console.log("로그인 error:", error);
     return res.status(StatusCodes.BAD_REQUEST).end();
   }
-}
-
+};
 
 //::카카오
-export const kakao  = async (req: Request, res: Response) => {
-  const {email, password} = req.body;
-  return res.json({email: email, password: password});
-}
-
+export const kakao = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  return res.json({ email: email, password: password });
+};
 
 //::구글
 export const google = async (req: Request, res: Response) => {
-  const {email, password} = req.body;
-  return res.json({email: email, password: password});
-}
+  const { email, password } = req.body;
+  return res.json({ email: email, password: password });
+};
+
 
 // export default {signup, login, kakao, google};
