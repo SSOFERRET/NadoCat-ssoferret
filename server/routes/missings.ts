eaca9e21f3dfa75bf80c1f9b931a817a16bd3d87
ssoferret <owosso1212@gmail.com>
2024-08-12 13:43:27 +0900

    Feat: 실종고양이 제보글 READ 함수 라우터에 추가

--- server/routes/missings.ts
+++ server/routes/missings.ts
@@ -1,19 +1,19 @@
 import express from "express";
 import { getMissingFavorites, postMissingFavorites, deleteMissingFavorites } from "../controller/missing/MissingsFavorites";
-import { getMissings } from "../controller/missing/Missings";
-import { createMissing, deleteMissing, getMissing, updateFoundState, updateMissing } from "../controller/missing/Missing";
-import { createMissingReport, deleteMissingReport, deleteMissingReportHandler, updateMissingReport, updateMissingReportCheck } from "../controller/missing/MissingReport";
+import { createMissing, deleteMissing, getMissing, updateFoundState, updateMissing } from "../controller/missing/Missings";
+import { createMissingReport, deleteMissingReport, deleteMissingReportHandler, getMissingReport, updateMissingReport, updateMissingReportCheck } from "../controller/missing/MissingReports";
 
 const router = express.Router();
 router.use(express.json());
 
-router.get("", getMissings);
+// router.get("", getMissings);
 router.get("/:postId", getMissing);
 router.post("", createMissing);
 router.delete("/:postId", deleteMissing);
 router.put("/:postId", updateMissing);
 router.patch("/:postId", updateFoundState);
 
+router.get("/:MissingId/reports/:postId", getMissingReport);
 router.post("/:postId/reports", createMissingReport);
 router.delete("/:MissingId/reports/:postId", deleteMissingReportHandler);
 router.put("/:missingId/reports/:postId", updateMissingReport);
