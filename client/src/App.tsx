baa9d74a5ec6ce6866f4dd345fb568462e8dea56
jeong922 <hwajeong922@naver.com>
2024-08-13 10:57:44 +0900

    Chore: tanstack/react-query 설치

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import MyPage from "./pages/MyPage";
 import React from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./api/queryClient";
 
 const App: React.FC = () => {
   return (
+  <QueryClientProvider client={queryClient}>
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<MyPage/>} />
     </Routes>
    </BrowserRouter>
+  </QueryClientProvider>
 
   )
 }
