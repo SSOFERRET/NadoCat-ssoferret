ebcfa88024a1dfc9291a1c02d55df4ee49494f4d
Minep <pmh@ge.kornu.ac.kr>
2024-08-13 19:02:02 +0900

    Feature: Signup, Login component, page, scss 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,50 @@
-import { BrowserRouter,Routes, Route } from "react-router-dom";
-import MyPage from "./pages/MyPage";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyPage from "./pages/MyPage";
 import { StreetCats } from "./pages/streetCat/StreetCat";
+import Login from "./pages/user/Login";
+import Signup from "./pages/user/Signup";
+import Layout from "./components/layout/Layout";
 
 const App: React.FC = () => {
   return (
-   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<MyPage/>} />
-      <Route path="/boards/street-cats" element={<StreetCats/>} />
-    </Routes>
-   </BrowserRouter>
-
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/users/my"
+          element={
+            <Layout>
+              <MyPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/boards/street-cats"
+          element={
+            <Layout>
+              <StreetCats />
+            </Layout>
+          }
+        />
+        <Route
+          path="/users/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route
+          path="/users/signup"
+          element={
+            <Layout>
+              <Signup />
+            </Layout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
