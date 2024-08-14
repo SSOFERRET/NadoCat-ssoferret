e2a77088871ecc427914b73efb46dd0c7a2b23a5
jeong922 <hwajeong922@naver.com>
2024-08-14 19:46:37 +0900

    Feat: 커뮤니티 게시판 목록 조회 무한스크롤 구현

--- client/src/pages/community/Community.tsx
+++ client/src/pages/community/Community.tsx
@@ -2,33 +2,36 @@ import React from "react";
 import "../../styles/scss/pages/community/community.scss";
 import PostList from "../../components/community/PostList";
 import useCommunity from "../../hooks/useCommunity";
+import { useIntersectionObserver } from "../../hooks/useIntersectionObserver";
 
 const Community = () => {
   const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useCommunity();
+
+  const moreRef = useIntersectionObserver(([entry]) => {
+    if (entry.isIntersecting) {
+      if (!hasNextPage) {
+        return;
+      }
+      fetchNextPage();
+    }
+  });
 
   if (isLoading) {
     return <div>loading...</div>;
   }
 
   return (
-    <section className="container">
+    <section className="community-container">
       <div className="category">
         <span>커뮤니티</span>
       </div>
 
       <PostList posts={data} />
 
-      <button
-        onClick={() => fetchNextPage()}
-        disabled={!hasNextPage || isFetchingNextPage}
-      >
-        {isFetchingNextPage
-          ? "Loading more..."
-          : hasNextPage
-          ? "Load More"
-          : "Nothing more to load"}
-      </button>
+      <div className="more" ref={moreRef}>
+        {isFetchingNextPage && <div>loading...</div>}
+      </div>
     </section>
   );
 };
