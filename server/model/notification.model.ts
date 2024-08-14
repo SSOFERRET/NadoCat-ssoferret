3b49d57578ef6eff09f8b053137bc743e52e7804
ssoferret <owosso1212@gmail.com>
2024-08-14 18:39:10 +0900

    Feat: 알람 대상 사용자 리스트 가져오는 함수 작성

--- server/model/notification.model.ts
+++ server/model/notification.model.ts
@@ -1,6 +1,6 @@
+import { Prisma } from "@prisma/client";
 import prisma from "../client";
 import { INotification } from "../controller/notification/Notifications";
-
 
 export const createNotification = async (notifications: INotification[]) => {
   return await prisma.notifications.createMany({
@@ -15,6 +15,28 @@ export const updateNotificationsIsReadByReceiver = async (receiver: Buffer) => {
     },
     data: {
       isRead: 1
+    }
+  })
+}
+
+export const getMissingReporters = async (
+  tx: Prisma.TransactionClient,
+  missingId: number
+) => {
+  return await tx.missingReports.findMany({
+    where: {
+      missingId
+    }
+  })
+}
+
+export const getMissingFavoriteAdders = async (
+  tx: Prisma.TransactionClient,
+  postId: number
+) => {
+  return await tx.missingFavorites.findMany({
+    where: {
+      postId
     }
   })
 }
