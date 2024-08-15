a04d88c22eeb6121e8d654e38f9a521aacc440ee
jeong922 <hwajeong922@naver.com>
2024-08-15 21:06:42 +0900

    Feat: 404 에러 페이지 구현

---
+++ client/src/components/error/ErrorNotFound.tsx
@@ -0,0 +1,12 @@
+import "../../styles/scss/components/error/errorNotFound.scss";
+import NotFound from "./NotFound";
+
+const ErrorNotFound = () => {
+  return (
+    <div className="error-not-found">
+      <NotFound />
+    </div>
+  );
+};
+
+export default ErrorNotFound;
