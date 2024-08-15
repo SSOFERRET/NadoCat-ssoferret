42535b59b2929b8c7e65c0246b4271f7f38839b2
ssoferret <owosso1212@gmail.com>
2024-08-15 20:23:56 +0900

    Feat: categoryId로 카멜케이스 카테고리 명  반환하는 함수 외 작성

--- server/constants/category.ts
+++ server/constants/category.ts
@@ -1,3 +1,5 @@
+import { TCategoryId } from "../types/category";
+
 export const CATEGORY = Object.freeze({
   COMMUNITIES: 1,
   EVENTS: 2,
@@ -5,3 +7,17 @@ export const CATEGORY = Object.freeze({
   MISSING_REPORTS: 4,
   STREET_CATS: 5,
 });
+
+export const getCategory = (categoryId: TCategoryId) => {
+  switch (categoryId) {
+    case CATEGORY.COMMUNITIES: return "communities";
+    case CATEGORY.EVENTS: return "events";
+    case CATEGORY.MISSINGS: return "missings";
+    case CATEGORY.MISSING_REPORTS: return "missingsReports";
+    case CATEGORY.STREET_CATS: return "streetCats"
+  }
+};
+
+const getCategoryUrlString = (category: ReturnType<typeof getCategory>) => category === getCategory(CATEGORY.STREET_CATS) ? "street-cats" : category;
+
+export const getCategoryUrlStringById = (categoryId: TCategoryId) => getCategoryUrlString(getCategory(categoryId));
