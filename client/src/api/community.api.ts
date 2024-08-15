ad9d4e62192487503242e4a4c2d1c754df8aa862
jeong922 <hwajeong922@naver.com>
2024-08-15 21:04:26 +0900

    Fix: src/api/community.api.ts 수정

--- client/src/api/community.api.ts
+++ client/src/api/community.api.ts
@@ -5,6 +5,7 @@ const LIMIT = 10;
 const SORT = "latest";
 
 export type Sort = "latest" | "views" | "likes";
+
 interface ICommunityPostsParams {
   pageParam: number;
   limit?: number;
@@ -26,19 +27,29 @@ export const getCommunityPosts = async ({
   limit,
   sort,
 }: ICommunityPostsParams): Promise<ICommunityPage> => {
-  const response = await httpClient.get(
-    `/boards/communities?limit=${limit ?? LIMIT}&cursor=${pageParam}&sort=${
-      sort ?? SORT
-    }`
-  );
-  return response.data;
+  try {
+    const response = await httpClient.get(
+      `/boards/communities?limit=${limit ?? LIMIT}&cursor=${pageParam}&sort=${
+        sort ?? SORT
+      }`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching community posts:", error);
+    throw error;
+  }
 };
 
 export const getCommunityDetail = async ({
   postId,
 }: CommunityDetailParams): Promise<ICommunity> => {
-  const response = await httpClient.get(`/boards/communities/${postId}`);
-  return response.data;
+  try {
+    const response = await httpClient.get(`/boards/communities/${postId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching community detail:", error);
+    throw error;
+  }
 };
 
 export const getCommunityComments = async ({
@@ -46,10 +57,16 @@ export const getCommunityComments = async ({
   pageParam,
   limit,
 }: CommunityCommentsParams) => {
-  const response = await httpClient.get(
-    `/boards/communities/${postId}/comments?limit=${
-      limit ?? LIMIT
-    }&cursor=${pageParam}`
-  );
-  return response.data;
+  try {
+    const response = await httpClient.get(
+      `/boards/communities/${postId}/comments?limit=${
+        limit ?? LIMIT
+      }&cursor=${pageParam}`
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching community comments:", error);
+    throw error;
+  }
 };
