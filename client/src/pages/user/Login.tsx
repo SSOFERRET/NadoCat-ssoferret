3f306ff84262358a588c6660fac47c2e8d3f2a92
Minep <pmh@ge.kornu.ac.kr>
2024-08-17 20:22:17 +0900

    Feature:Login 컴포넌트, scss 생성

--- client/src/pages/user/Login.tsx
+++ client/src/pages/user/Login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Login.scss";
+import "../../styles/scss/pages/user/login.scss";
 
 const KAKAO_AUTH_URL = `${import.meta.env.VITE_KAKAO_AUTH_URL}?response_type=code&client_id=${import.meta.env.VITE_KAKAO_REST_API_KEY}&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}&scope=profile_nickname,profile_image,account_email`;
 
