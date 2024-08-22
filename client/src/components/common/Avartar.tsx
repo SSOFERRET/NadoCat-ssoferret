898cf609d662130117f1d47128761459edcb54eb
jeong922 <hwajeong922@naver.com>
2024-08-23 01:46:28 +0900

    Fix: 프로필 이미지 관련 수정

--- client/src/components/common/Avartar.tsx
+++ client/src/components/common/Avartar.tsx
@@ -1,21 +1,15 @@
-import React from "react";
 import "../../styles/scss/components/common/avartar.scss";
+import DefaultProfile from "../../assets/img/profileCat.png";
 
 interface IProps {
   profileImage: string | null;
   nickname: string;
 }
 
-const DEFAULT_PROFILE: string = "/src/assets/img/profileImg1.png";
-
 const Avartar = ({ profileImage, nickname }: IProps) => {
-
   return (
     <div className="avatar">
-      {/* {profileImage && <img src={profileImage} alt={nickname} />} */}
-      {!profileImage && (
-        <img src={DEFAULT_PROFILE} alt={nickname} className="default_profile" />
-      )}
+      <img src={profileImage ?? DefaultProfile} alt={nickname} />
     </div>
   );
 };
