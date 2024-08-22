f6ebcbe5e2d936fe572e19094ec27129fc509ee3
ssoferret <owosso1212@gmail.com>
2024-08-22 10:41:06 +0900

    Feat: 프로필 이미지가 없을 때를 위한 기본값 설정

--- client/src/components/common/Avartar.tsx
+++ client/src/components/common/Avartar.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import "../../styles/css/components/common/avartar.css";
+import "../../styles/scss/components/common/avartar.scss";
 
 interface IProps {
   profileImage: string | null;
   nickname: string;
 }
 
+const DEFAULT_PROFILE: string = "/src/assets/img/profileImg1.png";
+
 const Avartar = ({ profileImage, nickname }: IProps) => {
   return (
     <div className="avatar">
-      {profileImage && <img src={profileImage} alt={nickname} />}
+      {/* {profileImage && <img src={profileImage} alt={nickname} />} */}
+      {!profileImage && (
+        <img src={DEFAULT_PROFILE} alt={nickname} className="default_profile" />
+      )}
     </div>
   );
 };
