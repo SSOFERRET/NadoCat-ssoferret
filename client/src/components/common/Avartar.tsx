6cd4ced1a11057c0d350c964a2722e5ebc42153a
ssoferret <owosso1212@gmail.com>
2024-08-23 11:35:57 +0900

    Feat: onClick 기능 사용자 프로필 페이지로 이동 기능 작성

--- client/src/components/common/Avartar.tsx
+++ client/src/components/common/Avartar.tsx
@@ -4,14 +4,14 @@ import "../../styles/scss/components/common/avartar.scss";
 interface IProps {
   profileImage: string | null;
   nickname: string;
+  onClick?: () => void;
 }
 
 const DEFAULT_PROFILE: string = "/src/assets/img/profileImg1.png";
 
-const Avartar = ({ profileImage, nickname }: IProps) => {
-
+const Avartar = ({ profileImage, nickname, onClick }: IProps) => {
   return (
-    <div className="avatar">
+    <div className="avatar" onClick={onClick}>
       {/* {profileImage && <img src={profileImage} alt={nickname} />} */}
       {!profileImage && (
         <img src={DEFAULT_PROFILE} alt={nickname} className="default_profile" />
