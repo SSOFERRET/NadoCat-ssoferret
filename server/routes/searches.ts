1ccfb845b9589e6983b5230d8c267a043d44fa68
ssoferret <owosso1212@gmail.com>
2024-08-29 00:04:49 +0900

    Fix: 배포할 때 opensearch 클러스터 설치에 문제 생겨 opensearch 삭제

--- server/routes/searches.ts
+++ server/routes/searches.ts
@@ -1,10 +1,7 @@
 import express from "express";
-import { searchDocuments, searchDocumentsPagination } from "../controller/search/Searches";
 
 const router = express.Router();
 router.use(express.json());
 
-router.get("", searchDocuments);
-router.get("", searchDocumentsPagination);
 
 export default router;
