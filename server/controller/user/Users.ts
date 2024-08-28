b083d50f7d22afb858dc8af58885778eb1f0c808
Minep <pmh@ge.kornu.ac.kr>
2024-08-28 15:44:47 +0900

    Refactor: 유저페이지쪽 버튼에 채팅기능 연결

--- server/controller/user/Users.ts
+++ server/controller/user/Users.ts
@@ -42,10 +42,10 @@ export const signup = async (req: Request, res: Response) => {
 export const login = async (req: Request, res: Response) => {
   const { email, password, autoLogin } = req.body;
   const isAutoLogin = (autoLogin === 'true' || autoLogin === true); 
-  // const generalTokenMaxAge = parseInt(process.env.GENERAL_TOKEN_MAX_AGE || '300000'); // 5분
-  // const refreshTokenMaxAge = parseInt(process.env.REFRESH_TOKEN_MAX_AGE || "604800000");// 7일
-  const generalTokenMaxAge = 1 * 60 * 1000; // 1분
-  const refreshTokenMaxAge = 5 * 60 * 1000;// 5분
+  const generalTokenMaxAge = parseInt(process.env.GENERAL_TOKEN_MAX_AGE || '300000'); // 5분
+  const refreshTokenMaxAge = parseInt(process.env.REFRESH_TOKEN_MAX_AGE || "604800000");// 7일
+  // const generalTokenMaxAge = 10 * 60 * 1000; // 1분
+  // const refreshTokenMaxAge = 60 * 60 * 1000;// 5분
   // const refreshTokenMaxAge = 7 * 24 * 60 * 60 * 1000;// 7일
 
   try {
