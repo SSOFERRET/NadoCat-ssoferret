4e05448faea9ef19ac10f29bbf97f45d218fc7de
jeong922 <hwajeong922@naver.com>
2024-08-28 20:49:52 +0900

    Fix: 배경 로고 클릭 시 홈으로 이동하도록 수정

--- client/src/components/common/HomeCat.tsx
+++ client/src/components/common/HomeCat.tsx
@@ -1,11 +1,13 @@
 import "../../styles/scss/components/common/homeCat.scss";
 import pawLogo from "../../assets/img/pawLogo.png";
 import welcomeCat from "../../assets/img/welcomeCat2.png";
+import { useNavigate } from "react-router-dom";
 
 const HomeCat = () => {
+  const navigate = useNavigate();
   return (
     <div className="welcome-container">
-      <img className="logo" src={pawLogo} alt="나도있어고양이" />
+      <img className="logo" src={pawLogo} alt="나도있어고양이" onClick={() => navigate("/")} />
 
       <div className="title-container">
         <span>애묘인이 모이는 공간</span>
