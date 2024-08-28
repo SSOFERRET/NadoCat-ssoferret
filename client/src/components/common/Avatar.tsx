71fdd6e8e48ff12596384d145a3d6b8e157c3a6d
jeong922 <hwajeong922@naver.com>
2024-08-28 12:48:40 +0900

    Feat: Avatar.tsx 이미지 지연로딩 구현

--- client/src/components/common/Avatar.tsx
+++ client/src/components/common/Avatar.tsx
@@ -1,5 +1,6 @@
 import "../../styles/scss/components/common/avatar.scss";
 import DefaultProfile from "../../assets/img/profileImg1.png";
+import { useIntersectionObserver } from "../../hooks/useIntersectionObserver";
 
 interface IProps {
   profileImage: string | null;
@@ -9,9 +10,22 @@ interface IProps {
 }
 
 const Avatar = ({ profileImage, nickname, onClick, size = "sm" }: IProps) => {
+  const imageRef = useIntersectionObserver(([entry]) => {
+    if (entry.isIntersecting) {
+      const target = entry.target;
+      if (target instanceof HTMLImageElement) {
+        const dataSrc = target.dataset.src;
+        console.log(dataSrc);
+        if (dataSrc) {
+          target.src = dataSrc;
+        }
+      }
+    }
+  });
+
   return (
     <div className={`avatar ${size} ${onClick ? "pointer" : ""}`} onClick={onClick}>
-      <img src={profileImage || DefaultProfile} alt={nickname} />
+      <img ref={imageRef} data-src={profileImage || DefaultProfile} alt={nickname} />
     </div>
   );
 };
