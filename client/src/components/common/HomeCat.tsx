0abdd4ca548db913dbc311a98de9ec1c8d4c4125
jeong922 <hwajeong922@naver.com>
2024-08-29 14:24:46 +0900

    Fix: 배경 고양이 이미지 변경

--- client/src/components/common/HomeCat.tsx
+++ client/src/components/common/HomeCat.tsx
@@ -1,6 +1,6 @@
 import "../../styles/scss/components/common/homeCat.scss";
 import pawLogo from "../../assets/img/pawLogo.png";
-import welcomeCat from "../../assets/img/welcomeCat2.png";
+import welcomeCat from "../../assets/img/home.png";
 import { useNavigate } from "react-router-dom";
 
 const HomeCat = () => {
@@ -13,14 +13,7 @@ const HomeCat = () => {
         <span>애묘인이 모이는 공간</span>
         <span>나도있어고양이</span>
       </div>
-
-      <div className="rectangle">
-        <img src={welcomeCat} alt="welcomeCat" />
-        <div className="welcome-message">
-          <span>가입하라냥</span>
-        </div>
-      </div>
-      <div>태그 자리</div>
+      <img className="welcome-cat" src={welcomeCat} alt="welcomeCat" />
     </div>
   );
 };
