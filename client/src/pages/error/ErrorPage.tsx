0d008c43829b1b1de0f9019bea4634222e15d63b
jeong922 <hwajeong922@naver.com>
2024-08-29 18:26:45 +0900

    Design: ErrorPage UI 수정

--- client/src/pages/error/ErrorPage.tsx
+++ client/src/pages/error/ErrorPage.tsx
@@ -1,10 +1,16 @@
 import "../../styles/scss/pages/error/errorPage.scss";
 import NotFound from "../../components/error/NotFound";
+import HomeCat from "../../components/common/HomeCat";
 
 const ErrorPage = () => {
   return (
-    <div className="not-found-page">
-      <NotFound />
+    <div className="main-layout">
+      <HomeCat />
+      <section className="main-container">
+        <div className="not-found-page">
+          <NotFound />
+        </div>
+      </section>
     </div>
   );
 };
